81826751561fbde73823be13523c4d78126ee485
Edvinauskas <edvinas@tr0n-ubuntu32.(none)>
2013-01-20 22:09:31 +0200

    Removed file names with spaces

--- Nr. 1 - Python.py
+++
@@ -1,6 +0,0 @@
-multi_sum = 0
-for n in range(1000):
-    if n % 3 == 0 or n % 5 == 0:
-        multi_sum += n
-
-print multi_sum
--- Nr. 2 - Python.py
+++
@@ -1,9 +0,0 @@
-even_fib_sum = 0
-first = 1
-second = 1
-while second < 4000000:
-    first, second = second, (first + second)
-    if second % 2 == 0:
-        even_fib_sum += second
-
-print even_fib_sum
